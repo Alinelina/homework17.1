9ac9e23631f204a23b2594f46e9345a577b83919
alinelina <alinelina2013@gmail.com>
2022-07-03 10:40:22 +0300

    corrected comments again

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 const userDataInput = document.querySelector('#user');
 const userDataInputs = document.querySelectorAll('input[type="text"]');
-let dataset;
 
 function validate() {
-    let datasetString = userDataInput.value.trim();
-    dataset = datasetString.replace(/[^a-zа-яё\s\-\']/gi, '');
+    const datasetString = userDataInput.value.trim();
+    let dataset = datasetString.replace(/[^a-zа-яё\s\-\']/gi, '');
     dataset = dataset.split(' ');
     dataset = dataset.filter(elem => elem);
-    console.log(dataset);
+    return dataset;
 }
 
 function showResult() {
     for (let i = 0; i < 3; i++) {
-        let user = dataset[i];
-        let firstLetter = user[0];
+        let user = validate()[i];
+        const firstLetter = user[0];
         user = ` ${firstLetter.toUpperCase()}${user.slice(1).toLowerCase()}`;
-        userDataInputs[i+1].style.color = 'blue';
-        userDataInputs[i+1].value = user;
+        userDataInputs[i + 1].style.color = 'blue';
+        userDataInputs[i + 1].value = user;
     }
+}
 
 
-userDataInput.addEventListener("change", function () {
- 	validate();
-	showResult();
- });
+    userDataInput.addEventListener("change", function () {
+        validate();
+        showResult();
+    });
